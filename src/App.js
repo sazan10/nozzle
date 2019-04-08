d7b4726125430b5bed64e123df92130ac91303bf
sazan10 <070bex432@ioe.edu.np>
2019-04-08 12:21:40 +0545

    transformation on pushing button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Scene from './Scene';
+import ThreeScene from './ThreeScene';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Scene/>
+        <ThreeScene/>
       </div>
     );
   }
